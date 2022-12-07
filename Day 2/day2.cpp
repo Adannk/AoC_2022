c4513e848877aacb5b40fa9900276e418fa71bc8
Adam JK <adam.kusnierz@gmail.com>
2022-12-07 17:48:56 +0100

    Day2
    Shitcode but it works, will come back later and clean it up

---
+++ Day 2/day2.cpp
@@ -0,0 +1,258 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+int solution1();
+int solution2();
+
+#define ROCK 1
+#define PAPER 2
+#define SCISSORS 3
+
+int main()
+{
+    int firstPart = solution1();
+    int secondPart = solution2();
+    cout << "Part 1: " << firstPart << endl << "Part 2: " << secondPart << endl;
+    
+    return 0;
+}
+
+int solution1() {
+    int myHand = 0;
+    int opponentsHand = 0;
+    int solutionPart1 = 0;
+    ifstream InputFile;
+    InputFile.open("Input/input.txt",ios::in);
+    char c;
+
+    if(InputFile.is_open()) 
+    {
+        while(InputFile.good()) 
+        {
+            InputFile.get(c);
+            if (c == ' ')
+                continue;
+            
+            if(c == 'A')
+                opponentsHand = ROCK;         //Rock
+            if(c == 'B')
+                opponentsHand = PAPER;         //Paper
+            if(c == 'C')
+                opponentsHand = SCISSORS;         //Scissors
+
+            if(c == 'X')
+                myHand = ROCK;         //Rock
+            if(c == 'Y')
+                myHand = PAPER;         //Paper
+            if(c == 'Z')
+                myHand = SCISSORS;         //Scissors
+
+            
+            
+            if(c == '\n')
+            {
+                //****************************************
+                if((myHand == ROCK) && (opponentsHand == ROCK))
+                {
+                    solutionPart1 += myHand;
+                    myHand = 0;
+                    opponentsHand = 0;
+                }
+                else if((myHand == ROCK) && (opponentsHand == PAPER))
+                {
+                    solutionPart1 += myHand;
+                    myHand = 0;
+                    opponentsHand = 0;
+                
+                }
+                else if((myHand == ROCK) && (opponentsHand == SCISSORS))
+                {
+                    myHand += 6;
+                    solutionPart1 += myHand;
+                    myHand = 0;
+                    opponentsHand = 0;
+                }
+                //****************************************
+                if((myHand == PAPER) && (opponentsHand == PAPER))
+                {
+                    myHand += 3;
+                    solutionPart1 += myHand;
+                    myHand = 0;
+                    opponentsHand = 0;
+                }
+                else if((myHand == PAPER) && (opponentsHand == SCISSORS))
+                {
+                    solutionPart1 += myHand;
+                    myHand = 0;
+                    opponentsHand = 0;
+                
+                }
+                else if((myHand == PAPER) && (opponentsHand == ROCK))
+                {
+                    myHand += 6;
+                    solutionPart1 += myHand;
+                    myHand = 0;
+                    opponentsHand = 0;
+                }
+                //****************************************
+                if((myHand == SCISSORS) && (opponentsHand == SCISSORS))
+                {
+                    myHand += 3;
+                    solutionPart1 += myHand;
+                    myHand = 0;
+                    opponentsHand = 0;
+                }
+                else if((myHand == SCISSORS) && (opponentsHand == ROCK))
+                {
+                    solutionPart1 += myHand;
+                    myHand = 0;
+                    opponentsHand = 0;
+                
+                }
+                else if ((myHand == SCISSORS) && (opponentsHand == PAPER))
+                {
+                    myHand += 6;
+                    solutionPart1 += myHand;
+                    myHand = 0;
+                    opponentsHand = 0;
+                }
+                //****************************************
+            }
+        }
+    }
+
+    if(!InputFile.eof() && InputFile.fail())
+        cout << "error reading file" << endl;
+
+    InputFile.close();
+    return solutionPart1;
+}
+
+int solution2() {
+    int myHand = 0;
+    int opponentsHand = 0;
+    int solutionPart2 = 0;
+    ifstream InputFile;
+    InputFile.open("Input/input.txt",ios::in);
+    char c;
+
+    if(InputFile.is_open()) 
+    {
+        while(InputFile.good()) 
+        {
+            InputFile.get(c);
+            if (c == ' ')
+                continue;
+            
+            if(c == 'A')
+                opponentsHand = ROCK;         //Rock
+            if(c == 'B')
+                opponentsHand = PAPER;         //Paper
+            if(c == 'C')
+                opponentsHand = SCISSORS;         //Scissors
+
+            if(c == 'X')
+                myHand = ROCK;         //Rock
+            if(c == 'Y')
+                myHand = PAPER;         //Paper
+            if(c == 'Z')
+                myHand = SCISSORS;         //Scissors
+
+            
+            
+            if(c == '\n')
+            {
+                //****************************************
+                if(myHand == ROCK)
+                {
+                    if(opponentsHand == PAPER)
+                    {
+                        myHand = ROCK;
+                        solutionPart2 += myHand;
+                        myHand = 0;
+                        opponentsHand = 0;
+                    }
+                    else if(opponentsHand == ROCK)
+                    {
+                        myHand = SCISSORS;
+                        solutionPart2 += myHand;
+                        myHand = 0;
+                        opponentsHand = 0;
+                    }
+                    else if(opponentsHand == SCISSORS)
+                    {
+                        myHand = PAPER;
+                        solutionPart2 += myHand;
+                        myHand = 0;
+                        opponentsHand = 0;
+                    }
+                }
+                //****************************************
+                if(myHand == PAPER)
+                {
+                    if(opponentsHand == PAPER)
+                    {
+                        myHand = PAPER;
+                        solutionPart2 += myHand;
+                        solutionPart2 += 3;
+                        myHand = 0;
+                        opponentsHand = 0;
+                    }
+                    else if(opponentsHand == ROCK)
+                    {
+                        myHand = ROCK;
+                        solutionPart2 += myHand;
+                        solutionPart2 += 3;
+                        myHand = 0;
+                        opponentsHand = 0;
+                    }
+                    else if(opponentsHand == SCISSORS)
+                    {
+                        myHand = SCISSORS;
+                        solutionPart2 += myHand;
+                        solutionPart2 += 3;
+                        myHand = 0;
+                        opponentsHand = 0;
+                    }
+                }
+                //****************************************
+                if(myHand == SCISSORS)
+                {
+                    if(opponentsHand == PAPER)
+                    {
+                        myHand = SCISSORS;
+                        solutionPart2 += myHand;
+                        solutionPart2 += 6;
+                        myHand = 0;
+                        opponentsHand = 0;
+                    }
+                    else if(opponentsHand == ROCK)
+                    {
+                        myHand = PAPER;
+                        solutionPart2 += myHand;
+                        solutionPart2 += 6;
+                        myHand = 0;
+                        opponentsHand = 0;
+                    }
+                    else if(opponentsHand == SCISSORS)
+                    {
+                        myHand = ROCK;
+                        solutionPart2 += myHand;
+                        solutionPart2 += 6;
+                        myHand = 0;
+                        opponentsHand = 0;
+                    }
+                }
+                //****************************************
+            }
+        }
+    }
+
+    if(!InputFile.eof() && InputFile.fail())
+        cout << "error reading file" << endl;
+
+    InputFile.close();
+    return solutionPart2;
+}
